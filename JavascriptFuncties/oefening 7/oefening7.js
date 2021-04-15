7127e0575a28cb5509f2bcc0d857ad6fc7575457
NickSoons <55700514+NickSoons@users.noreply.github.com>
2021-04-15 16:13:09 +0200

    oefening 7 part 1

---
+++ JavascriptFuncties/oefening 7/oefening7.js
@@ -0,0 +1,8 @@
+function alleWoordenStartenMetHoofdletter(){
+    let zin = "Deze oefeningen zijn héél leuk."
+    let uitkomst = "";
+
+    document.getElementById("uitkomst").innerHTML = uitkomst;
+
+}
+alleWoordenStartenMetHoofdletter();
