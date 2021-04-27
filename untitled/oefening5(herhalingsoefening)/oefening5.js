656b32c1ffbf1d715594ba3700a61636deac35d6
NickSoons <55700514+NickSoons@users.noreply.github.com>
2021-04-27 13:43:07 +0200

    herhalingsoef 1

--- untitled/oefening5(herhalingsoefening)/oefening5.js
+++ untitled/oefening5(herhalingsoefening)/oefening5.js
@@ -1,1 +1,18 @@
+let klanten = [
+    {voornaam: 'Jasper', naam: 'Beuls', stad: 'Hasselt'},
+    {voornaam: 'Kimberly', naam: 'Willems', stad: 'Genk'},
+    {voornaam: 'Nicolas', naam: 'Schepers', stad: 'Genk'},
+    {voornaam: 'Daniel', naam: 'Morren', stad: 'Diepenbeek'},
+    {voornaam: 'Doriene', naam: 'Verhoeven', stad: 'Oudsbergen'},
+];
 
+// oefening 1
+function filterGenk(klanten){
+    if (klanten.stad === "Genk"){
+        let personenGenk = Object.values(klanten);
+    }
+}
+
+//aanroepen functie 1
+filterGenk(klanten);
+console.log(personenGenk);
